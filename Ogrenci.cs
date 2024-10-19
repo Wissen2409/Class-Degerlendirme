923f727963c9c966f23e275bad94b93ef2109c25
emrahelis <emrahelis@gmail.com>
2024-10-19 13:15:19 +0300

    first commit

---
+++ Ogrenci.cs
@@ -0,0 +1,5 @@
+public class Ogrenci
+{
+    public int Id { get; set; }
+    public string Ad { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+// string hariç , her class referans tiplidir
+Ogrenci o = new Ogrenci();
+Console.WriteLine("Id : {0}",o.Id);
+Console.WriteLine("Ad : {0}",o.Ad);
+
+Degistir(o);
+Console.WriteLine("Id : {0}",o.Id);
+Console.WriteLine("Ad : {0}",o.Ad);
+
+// sadece ad alanını metoda gönderip, değer tip ve refernas tip kontrolü yapıyorum
+AdDegistir1(o.Ad);
+Console.WriteLine("Yeni  Ad : {0}",o.Ad);
+
+
+
+static void Degistir(Ogrenci o){
+    o.Id=10;
+    o.Ad="Kemalettin";
+}
+static void AdDegistir(Ogrenci o){
+    // bu senaryoda referans tipli davranışı sergilenecektir
+}
+static void AdDegistir1(string ad){
+    ad="Yavuz";
+}
+
+// 
